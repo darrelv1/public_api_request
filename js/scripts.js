0ef6a3b731e1c8b5cd0cec93b02cc0d7ecb77f07
Darrel Valdivieso <valdivieso4@hotmail.com>
2022-10-01 19:23:02 -0400

    deleted unnecessary templates

--- js/scripts.js
+++ js/scripts.js
@@ -54,7 +54,7 @@ function cardValidator(e){
 
 
 /**
- * Templates
+ * Template
  */
 const search_Markup =
     ` <form action="#" method="get">
@@ -62,46 +62,6 @@ const search_Markup =
         <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
     </form>`
 
-const gallery_Markup =
-    `
-    <div class="card">
-            <div class="card-img-container">
-                <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-            </div>
-            <div class="card-info-container">
-                <h3 id="name" class="card-name cap">first last</h3>
-                <p class="card-text">email</p>
-                <p class="card-text cap">city, state</p>
-            </div>
-    </div>
-    `
-
-const modal_Markup =
-    `
-              <div class="modal-container">
-                <div class="modal">
-                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                    <div class="modal-info-container">
-                        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-                        <h3 id="name" class="modal-name cap">name</h3>
-                        <p class="modal-text">email</p>
-                        <p class="modal-text cap">city</p>
-                        <hr>
-                        <p class="modal-text">(555) 555-5555</p>
-                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                        <p class="modal-text">Birthday: 10/21/2015</p>
-                    </div>
-                </div>
-    `
-
-const modal_btn_Markup =
-    `
-        <div class="modal-btn-container">
-            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-            <button type="button" id="modal-next" class="modal-next btn">Next</button>
-        </div>
-    </div>
-    `
 
 /**
  * Templates plugins
